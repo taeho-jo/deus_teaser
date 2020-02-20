3fb042a4b62c71f736f7d67df567c2d5d28b2f1a
jotang <jotang3726@gmail.com>
2020-02-20 16:43:59 +0900

    모달창 수정

--- src/pages/Partner/MainPartner.js
+++ src/pages/Partner/MainPartner.js
@@ -146,7 +146,7 @@ const MainPartner = ({ t, i18n, history }) => {
         <Modal
           error="Completed"
           color="#00ace6;"
-          please={t("subscibeMessage")}
+          please={t("subscribeMessage")}
           background={check}
           onClick={closed}
         />
